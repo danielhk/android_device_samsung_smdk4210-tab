b8f2dab9f5b3e32c39426b7004e9be2a6df2bbc6
danielhk <daniel.p6800@gmail.com>
2015-04-06 00:59:53 +0800

    Enable BOARD_CHARGER_SHOW_PERCENTAGE
    Percetage will be show at the center of image during charging

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,6 +172,7 @@ BOARD_SEPOLICY_UNION += \
 BOARD_CHARGING_MODE_BOOTING_LPM := "/sys/class/power_supply/battery/batt_lp_charging"
 BOARD_BATTERY_DEVICE_NAME := "battery"
 BOARD_CHARGER_RES := "device/samsung/smdk4210-tab/charger_res/images"
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 
 # Override healthd HAL
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.exynos4
