461c82043bd86265c45ff13779c7cbf21c4e8dc5
daniel_hk <daniel.p6800@gmail.com>
2015-08-05 16:26:36 +0800

    correct the config file name and path

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
 $(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
 
 # include a bunch of resources
