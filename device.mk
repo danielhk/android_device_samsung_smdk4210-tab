da362ec9bf81264ed138679a05625b277f72aa4a
daniel_hk <daniel.p6800@gmail.com>
2015-12-12 14:10:39 +0800

    Remove ROM related config
    1. the content of common_full_tablet_wifionly is repeated
    2. ROM related config file should not be in common device tree

--- device.mk
+++ device.mk
@@ -15,7 +15,6 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 $(call inherit-product-if-exists, vendor/samsung/smdk4210-tab/vendor.mk)
 
 # include a bunch of resources
